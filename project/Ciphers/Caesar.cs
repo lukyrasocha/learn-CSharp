70f15e7178ced09186b2237d08333fd3a6d18ff5
lukyrasocha <rasocha.lukas@gmail.com>
2023-01-10 17:22:50 +0100

    Encrpytion should be correct, decryption is not

--- project/Ciphers/Caesar.cs
+++ project/Ciphers/Caesar.cs
@@ -1,9 +1,56 @@
+using utils;
+
 namespace Ciphers {
 
     public class Caesar {
 
-        static void Main(string[] args){
-            Console.WriteLine("HELLO World");
+        public string[] encrypt(int key, string fileName){
+            string[] plainLines = ReadFile.ReadAllLines(fileName);
+            string[] encryptedLines = new string[plainLines.Length];
+
+            foreach (string line in plainLines){
+                string encryptedLine = "";
+                foreach(char character in line){
+                    if (character != ' '){
+                        int charCode = (int) character - 96;                  // To get a range 1-26 as in the alphabet;
+                        int encryptedCharCode = ((charCode + key) % 26) + 96; 
+
+                        encryptedLine += (char) encryptedCharCode;
+
+                    } else{
+                        encryptedLine += ' ';
+                    }
+
+                }
+
+                encryptedLines.Append(encryptedLine);
+            }
+            return encryptedLines;
         }
+
+        public string[] decrypt(int key, string fileName){
+            string[] encryptedLines = ReadFile.ReadAllLines(fileName);
+            string[] decryptedLines = new string[encryptedLines.Length];
+
+            foreach (string line in encryptedLines){
+                string decryptedLine = "";
+                foreach(char character in line){
+                    if (character != ' '){
+                        int charCode = (int) character - 96; // To get a range 1-26 as in an alphabet;
+                        int encryptedCharCode = (charCode - key) % 26; 
+
+                        decryptedLine += (char) encryptedCharCode;
+
+                    } else{
+                        decryptedLine += ' ';
+                    }
+
+                }
+
+                decryptedLines.Append(decryptedLine);
+            }
+            return decryptedLines;
+        }
+
     }
 }
