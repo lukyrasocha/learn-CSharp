b094f2190b02e145f31312ed567c20facf4266b2
lukyrasocha <rasocha.lukas@gmail.com>
2023-01-10 13:32:50 +0100

    Import basicsyntax package

--- basic/helloworld/Program.cs
+++ basic/helloworld/Program.cs
@@ -1,7 +1,17 @@
-﻿namespace HelloWorld {
-    class Program {
-        static void Main(string[] args){
-            Console.WriteLine("Hello World!");
-        }
+﻿using basicsyntax;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Hello World!");
+        test();
+
+    }
+
+    static void test() {
+        MyClass obj = new MyClass();
+        obj.func();
+        int c = obj.add(5,10);
+        Console.WriteLine(c);
+        obj.testSyntax();
     }
 }
